1ed1ba0120a6e8cdae6d888761bbf731b44fc112
Your Namebasit <basitali.vision@gmail.com>
2024-12-07 17:21:44 +0500

    Assingnment Completed

---
+++ func.js
@@ -0,0 +1,18 @@
+let buyerName =  "Basit";
+
+let buyAmount = 100;
+
+let discount = 10;  
+
+let discountAmount = (buyAmount * discount) / 100;
+
+let finalAmount = buyAmount - discountAmount;
+
+console.log("Buyer-Name:" + buyerName);
+
+console.log("Original Amount: Rs " + buyAmount);
+
+console.log("Discount Amount: Rs " + discountAmount);
+
+console.log("Final Amount:    Rs " + finalAmount);
+
